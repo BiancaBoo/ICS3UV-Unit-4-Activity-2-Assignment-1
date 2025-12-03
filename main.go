73fa5267909240c18a2f0ccaf97899a9222806ad
default <bianca.bertinato@stu.ocsb.ca>
2025-12-03 02:17:49 +0000

    added number sum with a set range

---
+++ main.go
@@ -0,0 +1,48 @@
+// Author: Bianca Boo
+// Version: 1.0.0
+// Date: 2025-12-02
+// Fileoverview: This sums integers within a set range.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var startRange, endRange int
+	var num int
+	sumInside := 0
+	sumOutside := 0
+
+	// Input start and end of range
+	fmt.Print("Enter an integer for the start of the range: ")
+	fmt.Scanln(&startRange)
+
+	fmt.Print("Enter an integer for the end of the range: ")
+	fmt.Scanln(&endRange)
+
+	for {
+		// Input number
+		fmt.Print("Enter an integer or zero (0) to end: ")
+		fmt.Scanln(&num)
+
+		// Stop if zero
+		if num == 0 {
+			break
+		}
+
+		// Check if number is inside or outside range
+		if num >= startRange && num <= endRange {
+			sumInside += num
+		} else {
+			sumOutside += num
+		}
+	}
+
+	// Output results
+	fmt.Println("The sum of the integers inside the range is", sumInside)
+	fmt.Println("The sum of the integers outside the range is", sumOutside)
+
+  fmt.Println("\nDone.")
+}
